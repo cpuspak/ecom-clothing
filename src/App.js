6f7ab729e26a75dcf025261c08b5d6d439f69612
cpuspak <chkrhn123@gmail.com>
2021-05-27 22:22:09 +0530

    Added shop page and component preview in shop page along with routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import {Switch, Route} from 'react-router-dom';
+
+
 import { HomePage } from './pages/HomePage';
+import ShopPage from './pages/shop/shop.component';
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route exact path='/shop' component={ShopPage} />
+      </Switch>
     </div>
   );
 }
