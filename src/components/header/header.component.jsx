f6d40be48904f8995fb2c33a1b2cb47d44d636af
cpuspak <chkrhn123@gmail.com>
2021-06-21 01:06:33 +0530

    implemented cart-icon, cart-dropdown and cart-reducer for the cart-dropdown popup onclick

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -5,11 +5,13 @@ import { ReactComponent as Logo} from '../../assets/crown.svg';
 import { auth } from '../../firebase/firebase.utils';
 
 import { connect } from 'react-redux';
+import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 
 
 import './header.style.scss';
 
-const Header = ({ currentUser }) => {
+const Header = ({ currentUser, hidden }) => {
     return(
         <div className="header">
             <Link to="/">
@@ -27,13 +29,21 @@ const Header = ({ currentUser }) => {
                     <div className="option" onClick={()=> auth.signOut()}>Sign Out</div> :
                     <Link className="option" to="/signin">Sign In</Link>
                 }
+                <CartIcon />
             </div>
+            {
+                hidden ? null :
+                <CartDropdown />
+            }   
         </div>
     )
 }
 
-const mapStateToProps = state => ({
-    currentUser: state.user.currentUser
+const mapStateToProps = ({user: {currentUser}, cart: {hidden}}) => ({ // destructuring all these values from state, state = {user:{currentUser} ...}
+    //currentUser: state.user.currentUser,
+    //currentUser: currentUser => next line is the short form of this same thing
+    currentUser,
+    hidden
 })
 
 export default connect(mapStateToProps)(Header);
--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,6 +1,9 @@
 import { combineReducers } from 'redux';
 
+
 import userReducer from '../redux/user/user.reducer';
+import cartReducer from './cart/cart.reducer';
 export default combineReducers ({
-    user: userReducer
+    user: userReducer,
+    cart: cartReducer
 });
